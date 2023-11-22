eab5df44259b3bf55d66791d2880e695eb4e7a8c
Samara Mendes da Silveira <146882205+samaramdss@users.noreply.github.com>
2023-11-22 10:21:49 -0300

    Add files via upload
    Aula 02

---
+++ aula02/aninhados.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "Andre",
+    idade: 32,
+    cpf: "111.222.333-44",
+    email: "andre@dominio.com.br",
+    telefone: ["4432253091", "44991810521"],
+
+};
+cliente.endereco = {
+    rua: "R. Tiete",
+    numero: "435",
+    apartamento: true,
+    complemento: "Apto 12",
+}
+
+console.log(cliente.endereco);
---
+++ aula02/tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Andre",
+    idade: 32,
+    cpf: "111.222.333-44",
+    email: "andre@dominio.com.br",
+    telefone: ["4432253091", "44991810521"],
+};
+
+console.log(cliente.telefone);
